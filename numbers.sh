cd40343e644157c34324b164ca115d1b39d5d5db
Melody Nguyen <melnguyen@chapman.edu>
2018-11-08 02:08:29 +0000

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#numbers.sh
+#melody
+
+echo "Enter a positive number"
+read NUMBER
+N=1
+
+while [ $N -le $NUMBER ]
+do
+	if  [ $((N%2)) -eq 0 ] 
+	then
+		echo "$N even"
+	else
+		echo "$N odd"
+	fi
+N=$((N+1))
+done
+
